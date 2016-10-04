26b8d272021b89abf2fb9f9d0d161a51aa97d166
cf904d31 <cf904d31@gmail.com>
2016-10-04 14:18:58 +0800

    新增一個相機預覽視窗的class V2

--- app/src/main/java/iii/org/tw/mysendfile/CameraPreview.java
+++ app/src/main/java/iii/org/tw/mysendfile/CameraPreview.java
@@ -5,26 +5,58 @@ import android.hardware.Camera;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.io.IOException;
+
 /**
  * Created by user on 2016/10/4.
  */
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private Camera mcamera;
+    private SurfaceHolder holder;
+
     public CameraPreview(Context context, Camera camera) {
         super(context);
+
+        mcamera = camera;
+
+        holder = getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-
+        try {
+            mcamera.setPreviewDisplay(holder);
+            mcamera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        if (holder.getSurface() == null) {
+            return;
+        }
 
+        try {
+            mcamera.stopPreview();
+        } catch (Exception e) {
+
+        }
+
+        try {
+            mcamera.setPreviewDisplay(holder);
+            mcamera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-
+        mcamera.release();
+        mcamera = null;
     }
 }
