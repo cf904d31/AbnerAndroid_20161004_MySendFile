e63101b0c285c75da2cad83f2506d6d3a17611f4
cf904d31 <cf904d31@gmail.com>
2016-10-04 11:50:06 +0800

    取得網路上的openData資料新增下載中~~~~

--- app/src/main/java/iii/org/tw/mysendfile/MainActivity.java
+++ app/src/main/java/iii/org/tw/mysendfile/MainActivity.java
@@ -1,6 +1,9 @@
 package iii.org.tw.mysendfile;
 
+import android.app.ProgressDialog;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,12 +18,21 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private File sdroot;
+    private ProgressDialog pDialog;
+    private UIHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sdroot = Environment.getExternalStorageDirectory();
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setProgress(ProgressDialog.STYLE_SPINNER);
+        pDialog.setTitle("下載中請稍後...");
+
+        handler = new UIHandler();
+
     }
 
     public void addFile(View v) {
@@ -52,19 +64,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getData(View v) {
+        pDialog.show();
         new Thread() {
             @Override
             public void run() {
                 try {
-                    MultipartUtility mu = new MultipartUtility("http://m.coa.gov.tw/OpenData/FarmerMarketData.aspx","UTF-8");
+                    MultipartUtility mu = new MultipartUtility("http://data.coa.gov.tw/Service/OpenData/EzgoAttractions.aspx","UTF-8");
                     List<String> ret = mu.finish();
                     for (String line : ret){
                         Log.v("brad", line.length() + ":" + line);
                     }
+                    handler.sendEmptyMessage(0);
                 } catch (IOException e) {
                     e.printStackTrace();
+                    handler.sendEmptyMessage(0);
                 }
             }
         }.start();
+    }
+
+    public void camera(View v) {
+
+    }
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            pDialog.dismiss();
+        }
     }
 }
